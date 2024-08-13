b518740be136d7eddd9e9b77dae98030be2a0cc3
Andreas Pauly <andreas@pauly.ws>
2024-08-13 13:56:23 +0200

    added stuff to make the pytorch experiment run

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+from setuptools import setup, find_packages
+
+"""TODO: Description"""
+
+__author__ = "Laarmann, Felix"
+__copyright__ = "TODO"
+
+setup(
+    name='NN Synthesis',
+    version='0.1',
+    description='Synthesis of NN',
+    author='Laarmann, Felix',
+    author_email='firstname.lastname@tu-dortmund.de',
+    packages=find_packages()
+)
